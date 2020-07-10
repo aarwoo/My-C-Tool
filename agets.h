6a21477b625ce4f17cc6650f8b936319dcd31f27
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2020-07-10 13:33:29 +0800

    Add files via upload

--- agets.h
+++ agets.h
@@ -1,36 +1,22 @@
-#ifndef AGETS
-#define AGETS
+﻿#ifndef agets_h
+#define agets_h
 #include<stdio.h>
 #include<stdlib.h>
-char * agets(FILE * src);
-/**
- *agets:read a line with auto allocate memory for return value from src (Not include '\n' at last)
- *If anything is error,it returns NULL
- */
-char * agets(FILE * src){
-	char * ret=NULL,*tmp_ret;
-	size_t ret_size=0;
-	int tmp;
-	for(tmp=fgetc(src);tmp!=EOF&&tmp!='\n';tmp=fgetc(src)){
-		ret_size=ret_size+1;
-		tmp_ret=realloc(ret,ret_size*sizeof(char));
-		if(tmp_ret==NULL){
-			free(ret);
-			return NULL;
-		}else{
-			ret=tmp_ret;
-			*(ret+ret_size-1)=(char)tmp;
-		}
-	}
-	ret_size=ret_size+1;
-	tmp_ret=realloc(ret,ret_size*sizeof(char));
-	if(tmp_ret==NULL){
-		free(ret);
+char * _agets(FILE * src,char * str,size_t size,int ch,char * tmp){
+	ch=fgetc(src);
+	tmp=realloc(str,(size+1)*sizeof(char));
+	if(tmp==NULL){
+		free(str);
 		return NULL;
+	}else if(((char)ch)=='\n'||ch==EOF){
+		*(tmp+size)='\0';
+		return tmp;
 	}else{
-		ret=tmp_ret;
-		*(ret+ret_size-1)='\0';
-		return ret;
+		*(tmp+size)=((char)ch);
+		return _agets(src,tmp,size+1,0,NULL);
 	}
+}
+char * agets(FILE * src){
+	return _agets(src,NULL,0,0,NULL);
 }
 #endif
